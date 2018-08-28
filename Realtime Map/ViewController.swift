65ca0ecba9c528a522712071f00b636b9156e054
Victor Idongesit <victoridongesit@gmail.com>
2018-08-28 16:36:52 +0100

    Initial Commit

---
+++ Realtime Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Realtime Map
+//
+//  Created by Victor Idongesit on 28/08/2018.
+//  Copyright © 2018 Victor Idongesit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
